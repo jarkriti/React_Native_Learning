f7ce9b770467144908845f4ab208b0d100038ba5
kriti.jar@docusign.com <kriti.jar@docusign.com>
2020-02-17 15:00:52 -0800

    third stage of learning

--- rn-complete-guide/App.js
+++ rn-complete-guide/App.js
@@ -3,18 +3,37 @@ import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
 
 export default function App() {
   return (
-    <View style={{padding:50}}>
-      <View style={{flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center'}}>
-        <TextInput placeholder="Course Goal" style={{borderColor:"black", borderWidth:1, padding: 10, width: '80%'}}/>
-        <Button title="ADD"/>
+
+
+    //1. PART-1 Basics of flexbox
+    //2. stylesheet using stylesheet.create
+    <View style={styles.screen}>
+      <View style={styles.inputContainer}>
+        <TextInput placeholder="Course Goal" style={styles.input} />
+        <Button title="ADD" />
       </View>
       <View>
 
       </View>
     </View>
-  );
+  )
+  /* flexbox example
+      <View
+      style={{padding: 50, flexDirection: 'row', width: '100%', height: 300, justifyContent: 'space-around', alignItems:'stretch'}}>
+        <View style={{backgroundColor:'red', flex:1,justifyContent:'center',alignItems:'center'}}></View>
+        <View style={{backgroundColor:'blue', flex:1,justifyContent:'center',alignItems:'center'}}></View>
+        <View style={{backgroundColor:'green', flex:2,justifyContent:'center',alignItems:'center'}}></View>
+      </View>
+    );
+    */
 }
 
 const styles = StyleSheet.create({
-  
+  screen: {
+    padding: 50
+  },
+  inputContainer: {
+    flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center'
+  },
+  input: { borderColor: "black", borderWidth: 1, padding: 10, width: '80%' }
 });
