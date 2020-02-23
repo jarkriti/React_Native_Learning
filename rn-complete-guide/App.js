db2096416e27f5fd2d1addb81b3dc08f52032d88
kriti.jar@docusign.com <kriti.jar@docusign.com>
2020-02-23 12:37:15 -0800

    practice app

--- rn-complete-guide/App.js
+++ rn-complete-guide/App.js
@@ -1,39 +1,63 @@
-import React from 'react';
-import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
+import React, { useState } from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  TextInput,
+  Button,
+  ScrollView,
+  FlatList
+} from "react-native";
+
+import GoalItem from './components/GoalItem';
+import GoalInput from './components/GoalInput';
 
 export default function App() {
+
+  const [courseGoals, setCourseGoals] =  useState([]);
+  const [isAddMode, setIsAddMode] = useState(false);
+
+  const addGoalHandler = enteredGoal => {
+    setCourseGoals(currentGoals => [
+      ...currentGoals,
+      {
+        //key: Math.random().toString(),
+        id: Math.random().toString(),
+        value: enteredGoal
+      }
+    ]);
+    setIsAddMode(false);
+  };
+
+  const removeGoalHandler = goalId =>{
+    setCourseGoals(currentGoals =>{
+      return currentGoals.filter((goal)=>goal.id !== goalId)
+    })
+  }
+
+  const cancelGoalAdditionHandler = () => {
+       setIsAddMode(false); 
+    
+}
+
   return (
-
-
-    //1. PART-1 Basics of flexbox
-    //2. stylesheet using stylesheet.create
     <View style={styles.screen}>
-      <View style={styles.inputContainer}>
-        <TextInput placeholder="Course Goal" style={styles.input} />
-        <Button title="ADD" />
-      </View>
-      <View>
-
-      </View>
+      <Button title="Add new goal" onPress={() => setIsAddMode(true)}/>
+      <GoalInput visible={isAddMode} onAddGoal={addGoalHandler} onCancel={cancelGoalAdditionHandler}/>
+      <FlatList
+        keyExtractor = {(item,index)=>item.id}
+        data={courseGoals}
+        renderItem={itemData => 
+          <GoalItem onDelete={removeGoalHandler} id={itemData.item.id} title={itemData.item.value}
+          />
+        }
+      />
     </View>
-  )
-  /* flexbox example
-      <View
-      style={{padding: 50, flexDirection: 'row', width: '100%', height: 300, justifyContent: 'space-around', alignItems:'stretch'}}>
-        <View style={{backgroundColor:'red', flex:1,justifyContent:'center',alignItems:'center'}}></View>
-        <View style={{backgroundColor:'blue', flex:1,justifyContent:'center',alignItems:'center'}}></View>
-        <View style={{backgroundColor:'green', flex:2,justifyContent:'center',alignItems:'center'}}></View>
-      </View>
-    );
-    */
+  );
 }
 
 const styles = StyleSheet.create({
   screen: {
     padding: 50
   },
-  inputContainer: {
-    flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center'
-  },
-  input: { borderColor: "black", borderWidth: 1, padding: 10, width: '80%' }
 });
